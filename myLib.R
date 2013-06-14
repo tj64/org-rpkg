7fc8d411a32c874e0c6e13e7be2360350e7ba84b
tj <tj@data-driven.de>
2013-06-14 19:05:07 +0200

    package ArDec and template file myLib.R added.

---
+++ myLib.R
@@ -0,0 +1,47 @@
+## * myLib.R
+##   :PROPERTIES:
+##   :author_and_copyright: My_Name
+##   :copyright_years: 2013
+##   :version:  0.9
+##   :licence:  GPL2
+##   :licence_url: http://www.gnu.org/licenses/
+##   :free_software: yes
+##   :author_mail_account: myname
+##   :author_mail_host: gmail.com
+##   :keywords: R  ESS
+##   :END:
+
+## ** Description
+
+## 'myLib.R' is a reference implementation, illustrating the conventions for
+## writing R packages with Org-mode. It shows the `outshine' approach, i.e.
+## development is actually done in a machine-executable =.R= file that is only
+## temporarily converted to Org-mode for comment-text editing and file export (to
+## text formatting backends or as R package).
+
+## ** Usage
+
+## ** ChangeLog
+
+## | version | author(s)       | date            |
+## |---------+-----------------+-----------------|
+## |     0.9 | Thorsten Jolitz | <2013-06-14 Fr> |
+
+## * Requires
+## * Mode Definitions
+## * Variables
+## ** Consts
+## ** Vars
+## ** Hooks
+## ** Customs
+## * Functions
+## ** Non-interactive Functions
+
+
+## ** Commands
+
+
+## * Menus and Keys
+## ** Menus
+## ** Keys
+## * Run hooks and provide
